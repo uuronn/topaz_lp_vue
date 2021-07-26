63c53001d497b9324705e4924004ef0b1340bda4
uuronn <uuronprogram@gmail.com>
2021-07-26 18:15:08 +0900

    scss導入

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Mv/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Mv from './components/Mv.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Mv
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Mv.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mv">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mv {
+  background: $topaz-color;
+  width: 100%;
+  height: 680px;
+}
+</style>
