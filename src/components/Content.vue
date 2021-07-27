a14c130b18cdba4fecdb1ecbc55b894985d174e0
uuronn <uuronprogram@gmail.com>
2021-07-27 09:32:58 +0900

    nameChenge

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
+    <div class="container">
+      <div>
+        <img src="../assets/image/talk-img.png" alt="">
+      </div>
+      <div>
+        日頃
+      </div>
+    </div>
+    <div class="container">
 
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 div {
   width:100%;
-  height: 1000px;
   background: #fff;
 }
 </style>
--- src/components/Mv.vue
+++ src/components/Mv.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="mv">
-    <div class="wrapper">
-      <img class="fdfd" src="../assets/image/mv_hackz-img.png" alt="">
+    <div class="mv__topLogo">
+      <img class="topLogo__img" src="../assets/image/mv_hackz-img.png" alt="">
     </div>
-    <div class="container">
-      <div>
-        <div>
-          <img src="../assets/image/topaz-img.png" alt="">
-        </div>
-        <p>エンジニアのinput・outputを可視化する</p>
-        <button>使ってみる⇀</button>
+    <div class="mv__mainForm">
+      <div class="mv__mainLogo">
+        <img class="mainLogo__img" src="../assets/image/topaz-img.png" alt="">
       </div>
-      <div>
-        <img src="../assets/image/mv_mix-img.png" alt="">
-      </div>
+      <p class="mv__text">エンジニアのinput・outputを可視化する</p>
+      <button class="mv__btn">使ってみる⇀</button>
     </div>
-    <div></div>
-    <div class="mv__wrapper">
-      <img src="../assets/image/arrow.png" alt="">
+    <div class="mv__mixSample">
+      <img class="mixSample__img" src="../assets/image/mv_mix-img.png" alt="">
+    </div>
+    <div class="mv__bottomArrow">
+      <img class="bottomArrow__img" src="../assets/image/arrow.png" alt="">
     </div>
   </div>
 </template>
@@ -36,17 +33,11 @@ export default {
 .mv {
   background: $topaz-color;
   width: 100%;
-}
 
-.container {
-  display: flex;
-}
-
-.mv__wrapper {
-  display: flex;
-  flex-direction: column-reverse;
-  img {
-    width: 200px;
+  &__topLogo {
+    .topLogo__img {
+      
+    }
   }
 }
 </style>
