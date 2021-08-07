3f8c33d5097440ac641ab909316cace4bd4a1ba1
uuronn <uuronprogram@gmail.com>
2021-08-07 14:26:50 +0900

    topazImgs_feature

--- src/components/TopazImgs.vue
+++ src/components/TopazImgs.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="Imgs">
-    <img class="Imgs__profile" :src="images.pc" alt="プロフィール画像">
-    <img class="Imgs__sp show" :src="images.sp" width="220px" height="120px" alt="スマホ版画像">
-    <img class="Imgs__sp--small show" :src="images.spMini" widht="120px" height="240px" alt="スマホ版画像">
+  <div class="topazImgs">
+    <img class="topazImgs__pc" :src="images.pc" alt="パソコン版画像">
+    <img class="topazImgs__sp show" :src="images.sp" alt="スマホ版画像">
+    <img class="topazImgs__sp--small show" :src="images.spMini" alt="スマホ版画像">
   </div>
 </template>
 
@@ -11,9 +11,9 @@ export default {
   data() {
     return {
       images: {
-        pc: require("../assets/image/topaz_mix-img.png"),
-        sp: require("../assets/image/topaz_profile-img.png"),
-        spMini: require("../assets/image/topaz_profile-mini-img.png")
+        pc: require("../assets/image/topazImgs_pc-img.png"),
+        sp: require("../assets/image/topazImgs_sp-img.png"),
+        spMini: require("../assets/image/topazImgs_sp-small-img.png")
       }
     }
   }
@@ -21,35 +21,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.Imgs {
-  &__profile {
+.topazImgs {
+
+  // パソコン版画像
+  &__pc {
     width: 60vw;
     min-width: 320px;
     margin-left: 13vw;
   }
 
+  // スマホ版画像のスタイルまとめたもの
   .show {
     display: none;
   }
 
   @media screen and (max-width: 520px) {
-    &__profile {
+    &__pc {
       display: none;
     }
-
+    
     .show {
       display: block;
       position: absolute;
       z-index: 7;
     }
     
+    // スマホ版画像
     &__sp {
+      width: 220px;
+      height: 120px;
       right: -12%;
       top: 10%;
       transform: rotate(-10deg);
     }
 
+    // スマホ版画像 小さいほう
     &__sp--small {
+      width: 120px;
+      height: 240px;
       left: -5%;
       bottom: -4%;
       transform: rotate(24deg);
